4d7a363db76014d5e100a2c9a7bff080c936869b
Emmanuel Chimezie <echimezie@getvitaminc.com>
2015-08-26 17:11:16 -0400

    Fixed user name bug

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -8,6 +8,7 @@ class Listing < ActiveRecord::Base
 					  :dropbox_credentials => Rails.root.join("config/dropbox.yml"),
 					  :path => ":style/:id_:filename"
 	validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
+	belongs_to :user
 	end
 
 	#add validations
