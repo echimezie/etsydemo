c69172d35b645434657f91dd9990e764ab08af09
Emmanuel Chimezie <echimezie@getvitaminc.com>
2015-08-26 22:27:12 -0400

    ...

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -7,6 +7,7 @@ class Listing < ActiveRecord::Base
 					  :storage => :dropbox,
 					  :dropbox_credentials => Rails.root.join("config/dropbox.yml"),
 					  :path => ":style/:id_:filename"
+	validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 	belongs_to :user
 	end
 
