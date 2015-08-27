7d6b0e9092e2267cf820998e64a26f07785a212c
Emmanuel Chimezie <echimezie@getvitaminc.com>
2015-08-26 22:41:40 -0400

    ...

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -14,7 +14,7 @@ class Listing < ActiveRecord::Base
 	#add validations
 	validates :name, :description, :price, presence: true
 	validates :price, numericality: {greater_than: 0}
-	validates_attachment_presence :image
+	#validates_attachment_presence :image
 
 	#link to user
 	belongs_to :user
